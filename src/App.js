0753a1791cefa060b864df2826a053a6c0e80753
Diarmuid O Donovan <diarmuid@Diarmuids-Air.fritz.box>
2021-07-27 17:02:24 +0200

    added header component to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/ShopPage';
+import { Header } from './components/header/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
