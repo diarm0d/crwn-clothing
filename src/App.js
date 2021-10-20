bd188a47b206a5107930d16ce179ff1d7387800a
Diarmuid O Donovan <diarmuid.odonovan@protonmail.com>
2021-10-20 17:23:46 +0200

    added userAuth functionality to check current user and set state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/ShopPage';
 import { signInSignUpPage } from './pages/sign-in-sign-up/sign-in-sign-up';
 import { Header } from './components/header/Header';
-import { auth, createUserProfileDocument  } from './firebase/firebase';
+import { auth, createUserProfileDocument } from './firebase/firebase';
 import React from 'react';
 
 
@@ -20,9 +20,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
-      createUserProfileDocument(user);
-   })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => { 
+            console.log(this.state);
+          })
+        });    
+      } else {
+        this.setState({currentUser: userAuth});
+      }
+
+   });
   }
 
   componentWillUnmount() {
